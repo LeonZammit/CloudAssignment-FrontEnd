e5c4a3b52fd31ba4d30ca53f0c90055687c917e0
LeonZammit <80625286+LeonZammit@users.noreply.github.com>
2023-04-12 18:40:19 +0200

    Update script-api.js

--- js/script-api.js
+++ js/script-api.js
@@ -1,4 +1,4 @@
-const api_url = "https://api-dot-cloudassignment-383409.ew.r.appspot.com/";
+var api_url = "https://api-dot-cloudassignment-383409.ew.r.appspot.com/";
 function generateNumbers() {
     for (var i =0; i<10000; i++) {
         fetch(api_url + 'GenerateRandomNumber')
