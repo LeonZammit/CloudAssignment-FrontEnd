1f53ac732490151774fc1eb98490fecbda0f98f3
LeonZammit <80625286+LeonZammit@users.noreply.github.com>
2023-04-12 17:00:11 +0200

    Add files via upload

--- js/script-api.js
+++ js/script-api.js
@@ -1,4 +1,4 @@
-const api_url = "https://api-dot-cloudassignment-383409.ew.r.appspot.com";
+const api_url = "https://api-dot-cloudassignment-383409.ew.r.appspot.com/";
 
 function generateNumbers() {
     for (var i =0; i<10000; i++) {
