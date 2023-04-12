47067a238e44b138258e9342884a2e86016377b0
LeonZammit <80625286+LeonZammit@users.noreply.github.com>
2023-04-12 18:27:00 +0200

    Add files via upload

--- js/script-api.js
+++ js/script-api.js
@@ -1,11 +1,7 @@
 const api_url = "https://api-dot-cloudassignment-383409.ew.r.appspot.com/";
-
 function generateNumbers() {
     for (var i =0; i<10000; i++) {
-        fetch(api_url + 'GenerateRandomNumber', {
-            method: 'GET',
-            mode: 'no-cors'
-        })
+        fetch(api_url + 'GenerateRandomNumber')
             .then(response => {
                 console.log(response);
             })
